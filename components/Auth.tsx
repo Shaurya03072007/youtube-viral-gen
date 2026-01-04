070ad4b39850529d754c4526fa25acdb971e63be
Joropalli shaurya reddy <jshauryareddy@gmail.com>
2026-01-04 22:10:26 +0530

    Update Auth.tsx

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useAuth } from '../contexts/AuthContext';
+import AdminPage from './AdminPage';
 
 export default function Auth() {
 const username1 = ["shaurya"];
